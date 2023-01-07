33496dd2eb680e24604b5617e234b7db00c8dc3c
CelestialTios <geraud.bertrand@gmail.com>
2023-01-07 15:21:33 +0100

    First commit, the explorer import + Sally and Justine sound and .gitignore

---
+++ Assets/2DGamekit/Scripts/Audio/NestedRandomAudioPlayer.cs
@@ -0,0 +1,26 @@
+﻿using Gamekit2D;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace Assets._2DGamekit.Scripts.Audio
+{
+    public class NestedRandomAudioPlayer : RandomAudioPlayer
+    {
+
+        [Header("Clips for nested")]
+        public AudioClip beforeClip;
+        public AudioClip afterClip;
+
+        public void BeforePlaySound(TileBase surface = null)
+        {
+            m_Source.PlayOneShot(beforeClip);
+        }
+
+        public void AfterPlaySound(TileBase surface = null)
+        {
+            Debug.Log("Play After sound");
+            m_Source.PlayOneShot(afterClip);
+        }
+    }
+}
